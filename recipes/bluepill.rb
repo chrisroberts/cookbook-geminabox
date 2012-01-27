fae21175359aeb06db8c837029144d25c0cc6824
Chris Roberts <chrisroberts.code@gmail.com>
2012-01-27 15:33:39 -0800

    Add an upstart for bluepill

--- recipes/bluepill.rb
+++ recipes/bluepill.rb
@@ -4,10 +4,19 @@ gem_package('red_unicorn') do
   action :install
 end
 
+template '/etc/init/geminabox.conf' do
+  source 'upstart-geminabox-bluepill.erb'
+  variables(
+    :www_user => node[:geminabox][:www_user],
+    :base_directory => node[:geminabox][:base_directory]
+  )
+  notifies :restart, 'service[geminabox]'
+end
+
 template '/etc/bluepill/geminabox.pill' do
   source 'bluepill-geminabox.pill.erb'
   variables(
-    :pid => ,
+    :pid => File.join(node[:geminabox][:base_directory], 'unicorn.pid'),
     :working_directory => node[:geminabox][:base_directory],
     :exec => node[:geminabox][:unicorn][:exec],
     :config => File.join(node[:geminabox][:config_directory], 'unicorn.app'),
@@ -16,4 +25,11 @@ template '/etc/bluepill/geminabox.pill' do
     :maxmemory => node[:geminabox][:unicorn][:maxmemory],
     :maxcpu => node[:geminabox][:unicorn][:maxcpu]
   )
+  notifies :restart, 'service[geminabox]'
+end
+
+service 'geminabox' do
+  provider Chef::Provider::Service::Upstart
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
 end

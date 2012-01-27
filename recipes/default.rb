12bfec6e5aa4fa47826004e7b640df81b21a07a1
Chris Roberts <chrisroberts.code@gmail.com>
2012-01-27 15:35:02 -0800

    Create requried directories. Add rackup to base directory.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,17 @@
+# Ensure our directories exist
+directory node[:geminabox][:config_directory] do
+  action :create
+  recursive true
+  mode '0755'
+end
+
+directory File.join(node[:geminabox][:base_directory], node[:geminabox][:data_directory]) do
+  action :create
+  recursive true
+  mode '0755'
+  owner node[:geminabox][:www_user]
+end
+
 # Setup the frontend
 if(node[:geminabox][:nginx] || :this_is_all_we_support_now)
   include_recipe 'geminabox::nginx'
@@ -9,19 +23,22 @@ gem_package('geminabox') do
   version node[:geminabox][:version] || '~> 0.5.1'
 end
 
-# Configure up server instance
-if(node[:geminabox][:unicorn] || :this_is_all_we_support)
-  include_recipe 'geminabox::unicorn'
-end
-
 # Load up the monitoring
 if(node[:geminabox][:bluepill] || :this_is_all_we_support)
   include_recipe 'geminabox::bluepill'
 end
 
-template File.join(node[:geminabox][:config_directory], 'config.ru') do
+# Configure up server instance
+if(node[:geminabox][:unicorn] || :this_is_all_we_support)
+  include_recipe 'geminabox::unicorn'
+end
+
+template File.join(node[:geminabox][:base_directory], 'config.ru') do
   source 'config.ru.erb'
   variables(
-    :geminabox_data_directory => File.join(node[:geminabox][:base_directory], node[:geminabox][:data_directory])
+    :geminabox_data_directory => File.join(node[:geminabox][:base_directory], node[:geminabox][:data_directory]),
+    :geminabox_build_legacy => node[:geminabox][:build_legacy]
   )
+  mode '0644'
+  notifies :restart, 'service[geminabox]'
 end

3212b6e5178f3fcbf2cbbebb239427b640341b24
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-06-25 10:40:42 -0300

    Writes Unicorn PID file.
    PID file was not specified in Unicorn configuration so Bluepilld
    continuosly failed when looking up the service PID file.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chrisroberts.code@gmail.com"
 license          "Apache 2.0"
 description      "Installs and configures Geminabox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.3.0"
+version          "0.3.1"
 
 depends 'unicorn'
 depends 'rc_mon'
--- recipes/unicorn.rb
+++ recipes/unicorn.rb
@@ -17,6 +17,7 @@ unicorn_config File.join(node[:geminabox][:config_directory], 'geminabox.unicorn
   worker_processes node[:geminabox][:unicorn][:workers] || 2
   worker_timeout node[:geminabox][:unicorn][:timeout] || 30
   working_directory node[:geminabox][:base_directory]
+  pid ::File.join(node[:geminabox][:base_directory], 'unicorn.pid')
   preload_app true
   owner node[:geminabox][:www_user] || 'www-data'
   group node[:geminabox][:www_user] || 'www-data'
